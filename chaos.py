7c7143ef4b0217153a405a39c07b419e3df47ed2
ashlosbe <ashlosbe@mines.edu>
2018-12-05 20:38:54 -0700

    Update chaos.py

--- chaos.py
+++ chaos.py
@@ -11,3 +11,15 @@ def bifurcation_diagram(r_num,j_num):
     plt.show()
     
 bifurcation_diagram(1000,2000)
+
+def bifurcation_points(r_num,j_num):
+    log_map = [[2/3 for i in range(j_num)] for r in range(r_num)]
+    num_fixed = []
+    for r in range(r_num):
+        for j in range(j_num-1):
+            log_map[r][j+1] = (3+r/r_num)*log_map[r][j]*(1-log_map[r][j])
+        print(len(set(np.round(log_map[r][j+1],8))))
+        num_fixed.append(len(set(np.round(log_map[r][j+1],8))))
+    print(num_fixed)
+    
+bifurcation_points(1000,2000)
