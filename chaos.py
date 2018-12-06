1a1bf4ac0c699cf764f8f3737729121f34ea2aa8
ashlosbe <ashlosbe@mines.edu>
2018-12-05 20:22:49 -0700

    Update chaos.py

--- chaos.py
+++ chaos.py
@@ -1,11 +1,13 @@
 import numpy as np
-from matplotlib.pyplot import as plt
+from matplotlib import pyplot as plt
 
 def bifurcation_diagram(r_num,j_num):
     log_map = [[2/3 for i in range(j_num)] for r in range(r_num)]
-    r_vals = [3+r/1000 for r in range(r_num)]
     for r in range(r_num):
         for j in range(j_num-1):
             log_map[r][j+1] = (3+r/r_num)*log_map[r][j]*(1-log_map[r][j])
-        plt.scatter(r_vals,log_map[r][1000:],s=1)
+        r_vals = [3+r/1000 for j in range(j_num-1000)]
+        plt.scatter(r_vals,log_map[r][1000:],s=0.00001,c='b')
     plt.show()
+    
+bifurcation_diagram(1000,2000)
